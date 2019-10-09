87296812801470e94365ac0e0aa2c266d5374e07
mark4664 <mark@mybradley.me.uk>
2019-10-09 17:33:22 +0100

    Added a useless comment

--- readacard.py
+++ readacard.py
@@ -3,7 +3,7 @@ from time import sleep
 import sys
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
-
+# Spurious Comment!
 try:
     while True:
         print("Hold a tag near the reader")
